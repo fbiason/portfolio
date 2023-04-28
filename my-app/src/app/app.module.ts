bb54495cebbb39568b260da414031c0684951a07
biason franco <franco.biason@gmail.com>
2023-04-28 18:34:15 -0300

    28/4/23

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { ResumenComponent } from './resumen/resumen.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { CertificacionesComponent } from './certificaciones/certificaciones.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AboutComponent, HeaderComponent, ResumenComponent, ServiciosComponent, PortfolioComponent, CertificacionesComponent, ContactoComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
