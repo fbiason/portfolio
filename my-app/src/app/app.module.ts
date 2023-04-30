fdf0405b29c18924df96d8e8f5eff5573316c47d
biason franco <franco.biason@gmail.com>
2023-04-30 18:53:31 -0300

    30/4/23

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,14 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from './header/header.component';
-import { ResumenComponent } from './resumen/resumen.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
 import { ServiciosComponent } from './servicios/servicios.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { CertificacionesComponent } from './certificaciones/certificaciones.component';
 import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
-  declarations: [AppComponent, AboutComponent, HeaderComponent, ResumenComponent, ServiciosComponent, PortfolioComponent, CertificacionesComponent, ContactoComponent],
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HeaderComponent,
+    ExperienciaComponent,
+    ServiciosComponent,
+    PortfolioComponent,
+    CertificacionesComponent,
+    ContactoComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ my-app/src/app/experiencia/experiencia.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiencia',
+  templateUrl: './experiencia.component.html',
+  styleUrls: ['./experiencia.component.css'],
+})
+export class ExperienciaComponent {}
