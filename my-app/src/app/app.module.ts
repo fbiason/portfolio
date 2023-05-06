33fa253b0044728b02b3102a6ea2b930f12bd809
biason franco <franco.biason@gmail.com>
2023-05-06 17:43:51 -0300

    6.5.23

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
-import { HeaderComponent } from './header/header.component';
 import { ExperienciaComponent } from './experiencia/experiencia.component';
 import { ServiciosComponent } from './servicios/servicios.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { CertificacionesComponent } from './certificaciones/certificaciones.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'experiencia', component: ExperienciaComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'certificaciones', component: CertificacionesComponent },
+  { path: 'contacto', component: ContactoComponent },
+];
 
 @NgModule({
   declarations: [
@@ -21,8 +33,9 @@ import { ContactoComponent } from './contacto/contacto.component';
     PortfolioComponent,
     CertificacionesComponent,
     ContactoComponent,
+    HomeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
--- my-app/src/app/home/home.component.ts
+++ my-app/src/app/home/home.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
+
+import { AppRoutingModule } from '../app-routing.module';
+import { AppComponent } from '../app.component';
 import { AboutComponent } from '../about/about.component';
+import { HeaderComponent } from '../header/header.component';
+import { ExperienciaComponent } from '../experiencia/experiencia.component';
+import { ServiciosComponent } from '../servicios/servicios.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
+import { CertificacionesComponent } from '../certificaciones/certificaciones.component';
+import { ContactoComponent } from '../contacto/contacto.component';
 
 @Component({
   selector: 'app-home',
