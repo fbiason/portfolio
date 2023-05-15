905c744eb5a245e5ee20e4769e90f6affc6c730e
biason franco <franco.biason@gmail.com>
2023-05-15 19:52:18 -0300

    15.5.23

--- my-app/src/app/data.services.ts
+++ my-app/src/app/data.services.ts
@@ -1,1 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
+interface Experiencia {
+  nombre: string;
+  puesto: string;
+  fecha: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class DataServices {
+  experiencia: Experiencia[] = [];
+
+  constructor(private httpClient: HttpClient) {
+    this.CargarExperiencia();
+  }
+  private CargarExperiencia() {
+    this.httpClient
+      .get<Experiencia[]>(
+        'https://cv-biason-default-rtdb.firebaseio.com/Experiencia.json'
+      )
+      .subscribe((resp) => {
+        this.experiencia = resp;
+        console.log(resp);
+      });
+  }
+}
